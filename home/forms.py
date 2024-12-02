388738988f5b5b136d21231a614c9ccc3b729f68
PW_vers1 <l.c.limasud@gmail.com>
2024-12-02 19:24:07 -0300

    atividade

---
+++ home/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+
+
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nome', 'ordem']
+        widgets = {
+            'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Produto'}),
+            'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
+        }
+        
+
+
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Categoria
+from .models import *
+from .forms import *
 
 
 
@@ -13,3 +14,15 @@ def categoria(request):
     }
     return render(request, 'categoria/lista.html', contexto)
 
+def form_categoria(request):
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
+    contexto = {
+        'form':form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)
