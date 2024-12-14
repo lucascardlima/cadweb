341fc153c01aaff58b97ec4d692baceb7c60e75f
PW_vers1 <l.c.limasud@gmail.com>
2024-12-14 12:54:26 -0300

    Excluir

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria/', views.categoria, name="categoria"),
     path('categoria/form_categoria/', views.form_categoria, name="form_categoria"),
-    path('editar_categoria/<int:id>', views.editar_categoria, name='editar_categoria' ),
+    path('categoria/detalhes/<int:id>/', views.detalhes_categoria, name="detalhes_categoria"),
+    path('editar_categoria/<int:id>/', views.editar_categoria, name="editar_categoria"),
+    path('categoria/excluir/<int:id>/', views.excluir_categoria, name='excluir_categoria'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from .forms import *
 
@@ -10,7 +12,7 @@ def index(request):
 
 def categoria(request):
     contexto = {
-        'lista': Categoria.objects.all().order_by('id'),
+        'lista': Categoria.objects.all().order_by('-id'),
     }
     return render(request, 'categoria/lista.html', contexto)
 
@@ -27,6 +29,10 @@ def form_categoria(request):
     }
     return render(request, 'categoria/formulario.html', contexto)
 
+def detalhes_categoria(request, id):
+    categoria = get_object_or_404(Categoria, pk=id)
+    return render(request, 'categoria/detalhes.html', {'categoria': categoria})
+
 def editar_categoria(request, id):
     categoria = Categoria.objects.get(pk=id)
     if request.method == 'POST':
@@ -41,3 +47,14 @@ def editar_categoria(request, id):
          form = CategoriaForm(instance=categoria)
     return render(request, 'categoria/formulario.html', {'form': form,})
 
+def excluir_categoria(request, id):
+    if request.method == "DELETE":
+        try:
+            # Obtém o objeto Categoria ou retorna 404 se não encontrado
+            categoria = get_object_or_404(Categoria, pk=id)
+            categoria.delete()  # Exclui a categoria
+            return JsonResponse({'success': True}, status=200)  # Retorna resposta de sucesso
+        except Exception as e:
+            # Retorna erro em caso de falha
+            return JsonResponse({'success': False, 'error': str(e)}, status=500)
+    return JsonResponse({'success': False}, status=400)  # Caso não seja uma requisição DELETE
